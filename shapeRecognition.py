d3e4c2ae90112781fa24c616d1c0fc4e670ab525
Adamo O <adamo.orsini12@gmail.com>
2021-11-14 11:09:42 -0500

    Added shape and circle recognition

--- shapeRecognition.py
+++ shapeRecognition.py
@@ -6,4 +6,16 @@ class ShapeDetector:
 		# initialize the shape name and approximate the contour
 		shape = "unidentified"
 		peri = cv2.arcLength(c, True)
-		approx = cv2.approxPolyDP(c, 0.04 * peri, True)
+		vertices = cv2.approxPolyDP(c, 0.04 * peri, True)
+
+		if len(vertices) == 4:
+			x, y, width, height = cv2.boundingRect(vertices)
+			aspectRatio = float(width) / height
+
+			if aspectRatio >= 0.95 and aspectRatio <= 1.05:
+				shape = "square"
+
+		else:
+			shape = "circle"
+		return shape
+
