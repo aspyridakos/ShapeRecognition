9d0f9b9b28874a40d4da237de99e658a28628663
Adamo O <adamo.orsini12@gmail.com>
2021-11-14 10:52:11 -0500

    Fix comments and file name to match main

--- shapeRecognition.py
+++ shapeRecognition.py
@@ -1,25 +1,21 @@
 import numpy as np
 import cv2 as cv
 
+# Use video capture index 1 (0 = internal webcam, 1 = external webcam)
 capture = cv.VideoCapture(1)
 
 while(True):
       
     # Capture the video frame
-    # by frame
     ret, frame = capture.read()
   
     # Display the resulting frame
     if ret:
         cv.imshow('frame', frame)
       
-    # the 'q' button is set as the
-    # quitting button you may use any
-    # desired button of your choice
+    # Quit using 'q'
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
   
-# After the loop release the cap object
 capture.release()
-# Destroy all the windows
 cv.destroyAllWindows()
